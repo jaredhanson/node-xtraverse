1e0484f44b86823a4e13b2779747ecb2f61812f0
Jared Hanson <jaredhanson@gmail.com>
2013-07-29 22:49:43 -0700

    Improve test descriptions.

--- test/collection.test.js
+++ test/collection.test.js
@@ -7,11 +7,15 @@ describe('Collection', function() {
   var xml = fs.readFileSync(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8');
   var doc = $(xml);
   
-  it('root alias up to parent', function() {
+  it('should alias each to forEach', function() {
+    expect(doc.each).to.equal(doc.forEach);
+  });
+  
+  it('should alias up to parent', function() {
     expect(doc.up).to.equal(doc.parent);
   });
   
-  it('root alias prev to previous', function() {
+  it('should alias prev to previous', function() {
     expect(doc.prev).to.equal(doc.previous);
   });
   
