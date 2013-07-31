bf6fc451ba212d2644bdb240227aa58e30bd31a2
Jared Hanson <jaredhanson@gmail.com>
2013-07-31 11:25:46 -0700

    Set Travis CI script to make ci-travis.

--- support/mk/istanbul.mk
+++ support/mk/istanbul.mk
@@ -1,12 +1,13 @@
 ISTANBUL ?= istanbul
 ISTANBUL_OUT ?= ./reports/coverage
+ISTANBUL_REPORT ?= lcov
 ISTANBUL_HTML_REPORT_PATH ?= $(ISTANBUL_OUT)/lcov-report/index.html
 
 
 test-istanbul-mocha: node_modules
 	NODE_PATH=$(NODE_PATH_TEST) \
 	$(ISTANBUL) cover \
-	--dir $(ISTANBUL_OUT) \
+	--dir $(ISTANBUL_OUT) --report $(ISTANBUL_REPORT) \
 	$(_MOCHA) -- \
 		--reporter $(MOCHA_REPORTER) \
 		--require $(MOCHA_REQUIRE) $(TESTS)
