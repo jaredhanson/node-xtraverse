e4f6c9777b45fb2ced9f97cba9534de09f0dbd85
Jared Hanson <jaredhanson@gmail.com>
2013-08-15 20:32:28 -0700

    Don't use readFileSync in wrap tests.

--- test/wrap.test.js
+++ test/wrap.test.js
@@ -5,44 +5,56 @@ var fs = require('fs')
 
 describe('wrap', function() {
   
-  it('should wrap documents', function() {
-    var xml = fs.readFileSync(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8');
-    var doc = new DOMParser().parseFromString(xml);
-    var el = $(doc);
+  it('should wrap documents', function(done) {
+    fs.readFile(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8', function(err, xml) {
+      if (err) return done(err);
+      var doc = new DOMParser().parseFromString(xml);
+      var el = $(doc);
     
-    expect(el).to.have.length(1);
-    expect(el.name()).to.equal('parent');
+      expect(el).to.have.length(1);
+      expect(el.name()).to.equal('parent');
+      done();
+    });
   });
   
-  it('should wrap elements', function() {
-    var xml = fs.readFileSync(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8');
-    var doc = new DOMParser().parseFromString(xml);
-    var el = $(doc.documentElement.childNodes[1]);
+  it('should wrap elements', function(done) {
+    fs.readFile(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8', function(err, xml) {
+      if (err) return done(err);
+      var doc = new DOMParser().parseFromString(xml);
+      var el = $(doc.documentElement.childNodes[1]);
     
-    expect(el).to.have.length(1);
-    expect(el.name()).to.equal('child');
-    expect(el.attr('name')).to.equal('John');
+      expect(el).to.have.length(1);
+      expect(el.name()).to.equal('child');
+      expect(el.attr('name')).to.equal('John');
+      done();
+    });
   });
   
-  it('should strings of XML', function() {
-    var xml = fs.readFileSync(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8');
-    var el = $(xml);
+  it('should wrap strings of XML', function(done) {
+    fs.readFile(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8', function(err, xml) {
+      if (err) return done(err);
+      var el = $(xml);
     
-    expect(el).to.have.length(1);
-    expect(el.name()).to.equal('parent');
+      expect(el).to.have.length(1);
+      expect(el.name()).to.equal('parent');
+      done();
+    });
   });
   
-  it('should not rewrap wrapped nodes', function() {
-    var xml = fs.readFileSync(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8');
-    var doc = new DOMParser().parseFromString(xml);
-    var el = $(doc);
-    var reel = $(el);
+  it('should not rewrap wrapped nodes', function(done) {
+    fs.readFile(__dirname + '/data/parent-2children-3grandchildren.xml', 'utf8', function(err, xml) {
+      if (err) return done(err);
+      var doc = new DOMParser().parseFromString(xml);
+      var el = $(doc);
+      var reel = $(el);
     
-    expect(el).to.have.length(1);
-    expect(el.name()).to.equal('parent');
-    expect(reel).to.have.length(1);
-    expect(reel.name()).to.equal('parent');
-    expect(el).to.equal(reel);
+      expect(el).to.have.length(1);
+      expect(el.name()).to.equal('parent');
+      expect(reel).to.have.length(1);
+      expect(reel.name()).to.equal('parent');
+      expect(el).to.equal(reel);
+      done();
+    });
   });
   
   it('should wrap undefined argument', function() {
